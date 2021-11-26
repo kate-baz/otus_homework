d09eee9f5d5a2bf9bd62b5af6b3b1f926e645d46
Ekaterina <e.bazikalova@gmail.com>
2021-11-27 00:58:21 +1100

    ДЗ по коллекциям (часть 3) - добавлена проверка на уникальность номера счета

--- L11-Map_Set_hashcode_equals/bank/Bank.java
+++ L11-Map_Set_hashcode_equals/bank/Bank.java
@@ -1,16 +1,34 @@
 import java.util.*;
+import java.util.spi.AbstractResourceBundleProvider;
 
 public class Bank {
 
-    private static HashMap<String, Client> accounts = new HashMap<>();
-    private static HashMap<String, Set<Account>> clients = new HashMap<>();
+    private static HashMap<Account, Client> accounts = new HashMap<>();
+    private static HashMap<Client, Set<Account>> clients = new HashMap<>();
 
-    public Client findClient (String account) {
+    public Client findClient (Account account) {
         return accounts.get(account);
     }
 
-    public Set<Account> findAccount (String client) {
+    public Set<Account> findAccount (Client client) {
         return clients.get(client);
+    }
+
+    public static String getUniqueID() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+
+    public static Account createAccount(String accountID, Client client) {
+        Account newAccount = new Account(client, accountID);
+        if (accounts.containsKey(newAccount)) {
+            System.out.println("Duplicate Account ID number.");
+            return null;
+        }
+        accounts.put(newAccount, client);
+        client.addAccount(newAccount);
+        clients.put(client, client.getActiveAccounts());
+        return newAccount;
     }
 
     public static void main(String[] args) {
@@ -19,29 +37,17 @@ public class Bank {
         Client petrov = new Client("Петров", 45);
         Client sidorov = new Client("Сидоров", 21);
 
-        Account ivanovAccount = new Account(ivanov, "12345");
-        Account ivanovAccount2 = new Account(ivanov, "56789");
-        Account petrovAccount = new Account(petrov, "574837");
-        Account sidorovAccount = new Account(sidorov, "957647");
-
-        ivanov.addAccount(ivanovAccount);
-        ivanov.addAccount(ivanovAccount2);
-        petrov.addAccount(petrovAccount);
-        sidorov.addAccount(sidorovAccount);
-
-        clients.put(ivanov.getName(), ivanov.getActiveAccounts());
-        clients.put(petrov.getName(), petrov.getActiveAccounts());
-        clients.put(sidorov.getName(), sidorov.getActiveAccounts());
-
-        accounts.put(ivanovAccount.getAccountNumber(),ivanov);
-        accounts.put(ivanovAccount2.getAccountNumber(),ivanov);
-        accounts.put(petrovAccount.getAccountNumber(),petrov);
-        accounts.put(sidorovAccount.getAccountNumber(),sidorov);
-
+        String accountNumber = getUniqueID();
         Bank bank = new Bank();
-        System.out.println(bank.findAccount("Иванов"));
-        System.out.println(bank.findAccount("Петров"));
-        System.out.println(bank.findClient("12345"));
 
+        Account account1 = createAccount(accountNumber, ivanov);
+        createAccount(accountNumber, petrov);
+        createAccount(getUniqueID(), sidorov);
+        createAccount(getUniqueID(), petrov);
+        createAccount(getUniqueID(), sidorov);
+
+        System.out.println(bank.findAccount(ivanov));
+        System.out.println(bank.findAccount(petrov));
+        System.out.println(bank.findClient(account1));
     }
 }
